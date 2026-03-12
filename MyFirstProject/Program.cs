97ea2264f7f52805e84f6beab876444658277ef7
ashok <ashok@KAMEPALLIASHOK>
2026-03-12 22:27:23 +0530

    Added many write lins

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!, Modified line second time\nModified in GUI");
+Console.WriteLine("Hello, World!, Modified line second time\nModified in GUI");
+Console.WriteLine("Hello, World!, Modified line second time\nModified in GUI");
+Console.WriteLine("Hello, World!, Modified line second time\nModified in GUI");
+
